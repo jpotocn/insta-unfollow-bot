8c0d0877e1a0bcc5550f1b29bd0c56f1435250a4
Jan <jpotocn@gmail.com>
2020-02-01 12:28:30 +0100

    login data

--- src/src/main/java/Main.java
+++ src/src/main/java/Main.java
@@ -7,10 +7,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\jpotocn\\IdeaProjects\\InstaUnfollower\\src\\webDriver\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
 
-        driver.navigate().to("https://www.google.si");
+        driver.navigate().to("https://www.instagram.com");
+
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/article/div[2]/div[2]/p/a")).click();
+
+        driver.findElement(By.name("username")).sendKeys("_jpot_");
+        driver.findElement(By.name("password")).sendKeys("Popolnikoncert090895");
+        driver.findElement(By.xpath("//*[@id=\"react-root\"]/section/main/div/article/div/div[1]/div/form/div[4]/button")).click();
         driver.close();
     }
 
